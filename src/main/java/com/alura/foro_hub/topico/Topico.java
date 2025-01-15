dd68fd08dc4033b48e8f60635d53397220694448
elgaricito <edgarjaguirre@hotmail.com>
2025-01-14 22:48:52 -0300

    Se crean la clase Topico y se le agregan los constructores, y anotaciones para poder Mapearla en la Base de datos

---
+++ src/main/java/com/alura/foro_hub/topico/Topico.java
@@ -0,0 +1,88 @@
+package com.alura.foro_hub.topico;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Table(name = "topicos")
+@Entity(name = "Topico")
+public class Topico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titulo;
+    private String mensaje;
+    private LocalDateTime fechaDeCreacion;
+    private String status;
+    private String autor;
+    private String curso;
+
+    // Constructor
+    public Topico(DatosTopico datosTopico) {
+        this.titulo = datosTopico.titulo();
+        this.mensaje = datosTopico.mensaje();
+        this.fechaDeCreacion = datosTopico.fechaDeCreacion();
+        this.status = datosTopico.status();
+        this.autor = datosTopico.autor();
+        this.curso = datosTopico.curso();
+    }
+
+    // Getters y Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public LocalDateTime getFechaDeCreacion() {
+        return fechaDeCreacion;
+    }
+
+    public void setFechaDeCreacion(LocalDateTime fechaDeCreacion) {
+        this.fechaDeCreacion = fechaDeCreacion;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+}
