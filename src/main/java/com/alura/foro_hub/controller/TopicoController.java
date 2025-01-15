ae3107eb2fa1f2034d912ccf2a4933ba4c382d20
elgaricito <edgarjaguirre@hotmail.com>
2025-01-14 23:07:13 -0300

    Se agrega el Codigo HTTP GET que lista todos los topicos cargados

--- src/main/java/com/alura/foro_hub/controller/TopicoController.java
+++ src/main/java/com/alura/foro_hub/controller/TopicoController.java
@@ -7,10 +7,9 @@ import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/topicos")
@@ -24,5 +23,10 @@ public class TopicoController {
     @Transactional
     public void registrarTopico(@RequestBody @Valid DatosTopico datosTopico){
         topicoRepository.save(new Topico(datosTopico));
+    }
+
+    @GetMapping
+    public List<Topico> listadoDeTopicos(){
+        return topicoRepository.findAll();
     }
 }
