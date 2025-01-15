5cf54988b07514da34c685449dd309619a9d7072
elgaricito <edgarjaguirre@hotmail.com>
2025-01-14 23:59:17 -0300

    Se agregan dos metodos para verificar si existe un mensaje y un titulo ya guardado en la base de datos

--- src/main/java/com/alura/foro_hub/topico/TopicoRepository.java
+++ src/main/java/com/alura/foro_hub/topico/TopicoRepository.java
@@ -3,4 +3,10 @@ package com.alura.foro_hub.topico;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TopicoRepository extends JpaRepository<Topico,Long> {
+
+    // Método para verificar si existe un título con un ID diferente
+    boolean existsByTituloAndIdNot(String titulo, Long id);
+
+    // Método para verificar si existe un mensaje con un ID diferente
+    boolean existsByMensajeAndIdNot(String mensaje, Long id);
 }
