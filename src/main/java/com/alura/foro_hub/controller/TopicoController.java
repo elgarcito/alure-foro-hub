390ef1e008bb184f179f0db1870753f5d6f2c090
elgaricito <edgarjaguirre@hotmail.com>
2025-01-14 23:22:05 -0300

    Se agrega el Codigo HTTP GET con el cual ingresando un ID se puede obtener el buscado

--- src/main/java/com/alura/foro_hub/controller/TopicoController.java
+++ src/main/java/com/alura/foro_hub/controller/TopicoController.java
@@ -3,6 +3,7 @@ package com.alura.foro_hub.controller;
 import com.alura.foro_hub.topico.DatosTopico;
 import com.alura.foro_hub.topico.Topico;
 import com.alura.foro_hub.topico.TopicoRepository;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,5 +29,12 @@ public class TopicoController {
     @GetMapping
     public List<Topico> listadoDeTopicos(){
         return topicoRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Topico obtenerTopicoUsandoId(@PathVariable Long id){
+        Topico topico = topicoRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Topico not found with id: " + id));
+        return topico;
     }
 }
