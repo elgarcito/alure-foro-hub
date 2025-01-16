0a2fd319d1fa0ac7e6feca908ef38a1db91677e3
elgaricito <edgarjaguirre@hotmail.com>
2025-01-15 22:56:35 -0300

    Modificando las respuestas de los pedidos HTTP para que muestren los codigos correctos

--- src/main/java/com/alura/foro_hub/controller/TopicoController.java
+++ src/main/java/com/alura/foro_hub/controller/TopicoController.java
@@ -1,12 +1,13 @@
 package com.alura.foro_hub.controller;
 
-import com.alura.foro_hub.topico.DatosTopico;
-import com.alura.foro_hub.topico.Topico;
-import com.alura.foro_hub.topico.TopicoRepository;
+import com.alura.foro_hub.domain.topico.DatosTopico;
+import com.alura.foro_hub.domain.topico.Topico;
+import com.alura.foro_hub.domain.topico.TopicoRepository;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,33 +28,34 @@ public class TopicoController {
     }
 
     @GetMapping
-    public List<Topico> listadoDeTopicos(){
-        return topicoRepository.findAll();
+    public ResponseEntity<List<Topico>> listadoDeTopicos(){
+        return ResponseEntity.ok(topicoRepository.findAll());
     }
 
     @GetMapping("/{id}")
     @Transactional
-    public Topico obtenerTopicoUsandoId(@PathVariable Long id){
+    public ResponseEntity obtenerTopicoUsandoId(@PathVariable Long id){
         Topico topico = topicoRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Topico not found with id: " + id));
-        return topico;
+        return ResponseEntity.ok(topico);
     }
 
     @PutMapping("/{id}")
     @Transactional
-    public Topico actualizarTopicoUsandoId(@RequestBody @Valid DatosTopico datosTopicosParaActualizar,@PathVariable Long id){
+    public ResponseEntity actualizarTopicoUsandoId(@RequestBody @Valid DatosTopico datosTopicosParaActualizar,@PathVariable Long id){
         Topico topico = topicoRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Topico not found with id: " + id));
         topico.actualizarDatos(datosTopicosParaActualizar, topicoRepository);
-        return topico;
+        return ResponseEntity.ok(topico);
     }
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void borrarTopicoUsandoId(@PathVariable Long id){
+    public ResponseEntity borrarTopicoUsandoId(@PathVariable Long id){
         Topico topico = topicoRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Topico not found with id: " + id));
         topicoRepository.deleteById(id);
         System.out.println("Topico con id: "+ id +" borrado correctamente");
+        return ResponseEntity.noContent().build();
     }
 }
