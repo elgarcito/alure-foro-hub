697aceebea3d1a89a9eddf711c6e2590281f876c
elgaricito <edgarjaguirre@hotmail.com>
2025-01-15 22:59:52 -0300

    Creando la Entidad Usuarios en la cual realizamos la persistencia. Se utilizan las anotaciones para crear la tabla en la base de datos y se implementa user details para el posterior uso

---
+++ src/main/java/com/alura/foro_hub/domain/usuarios/Usuarios.java
@@ -0,0 +1,77 @@
+package com.alura.foro_hub.domain.usuarios;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Table(name = "usuarios")
+@Entity(name = "Usuario")
+public class Usuarios implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String login;
+    private String clave;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return clave;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+
+}
